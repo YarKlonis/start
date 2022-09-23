410a8a68a7b0f6f61ca9404838197195d5951492
YarKlonis <113667623+YarKlonis@users.noreply.github.com>
2022-09-23 22:23:56 +0300

    Update прямоугольник из циферок.py

--- прямоугольник из циферок.py
+++ прямоугольник из циферок.py
@@ -1,6 +1,6 @@
-a = int(input('введите значение длины: '))
-b = int(input('введите значение высоты: '))
+a = int(input('введите значение одной стороны: '))
+b = int(input('введите значение второй стороны: '))
 for i in range (1, b+1):
-    for j in range (a):
-        print((i, i + b * j) * j)
-#не работает
+    for j in range (i, a*b+1, b):
+        print(j)
+#не работает как надо
