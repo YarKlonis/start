33c74763751b8234e53373223a8d725a281e0ae6
YarKlonis <113667623+YarKlonis@users.noreply.github.com>
2022-09-23 22:44:32 +0300

    Update прямоугольник из циферок.py

--- прямоугольник из циферок.py
+++ прямоугольник из циферок.py
@@ -1,6 +1,6 @@
 a = int(input('введите значение одной стороны: '))
 b = int(input('введите значение второй стороны: '))
 for i in range (1, b+1):
+    print() #это было сложно
     for j in range (i, a*b+1, b):
-        print(j)
-#не работает как надо
+        print(j, end=' ')
